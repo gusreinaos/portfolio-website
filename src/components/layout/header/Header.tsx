0d07622e568b6475dfe75e7b51f357b09ad64d12
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2023-04-03 11:41:33 +0200

    feat: implement header component

---
+++ src/components/layout/header/Header.tsx
@@ -0,0 +1,31 @@
+import styles from './Header.module.css';
+
+export interface IHeader {}
+
+const Header: React.FC<IHeader> = () => {
+  return (
+    <header className={`row ${styles.header}`}>
+      
+      <div className={`col-3 ${styles.header__logo}`}>
+        ORG.
+      </div>
+
+      <div className={'col-9'}>
+        <ul className={styles.header__menu}>
+          <li className={styles.header__menu_item}>
+            <a href='#'>Home</a>
+          </li>
+          <li className={styles.header__menu_item}>
+            <a href='#'>About</a>
+          </li>
+          <li className={styles.header__menu_item}>
+            <a href='#'>Contact</a>
+          </li>
+        </ul>
+      </div>
+    
+    </header>
+  );
+};
+
+export default Header;
